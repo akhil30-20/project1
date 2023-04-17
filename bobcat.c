fc347c47099ad670462cb3ad4213a7e9006f637f
akhil30-20 <130199829+akhil30-20@users.noreply.github.com>
2023-04-16 19:54:55 -0700

    Add files via upload

--- bobcat.c
+++ bobcat.c
@@ -5,5 +5,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
-int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
+#define BUFFER 4096
+int main(int argc, char *argv[]) {
+  char buff[BUFFER];
+  int fd, br;
+  char *file;
+  file = NULL;
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-") != 0) {
+      file = argv[i];
+      fd = open(file, O_RDONLY);
+      if (fd == -1) {
+        warnx(" %s: No such file or directory", file);
+      }
+      while ((br = read(fd, buff, BUFFER)) > 0) {
+        write(STDOUT_FILENO, buff, br);
+      }
+    }
+  }
+  close(fd);
+  exit(1);
+  return EXIT_SUCCESS;
+}
